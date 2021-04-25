1f4ef35396e92e3c30ebc1c3364130581b959b12
Katherine <73628642+FyrbalRoketslam@users.noreply.github.com>
2021-04-24 21:02:28 -0600

    Base Dialogue System in
    sorry if it's a bit broken at the moment, I cant figure out how to test it

--- Flower Hour AR App/Assets/Scripts/Dialogue.cs
+++ Flower Hour AR App/Assets/Scripts/Dialogue.cs
@@ -1,11 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
-public class Dialogue
+public struct Line
 {
-    [TextArea(3, 20)]
-    public string[] sentences;
+    public Sprite lillian;
 
+    [TextArea(3, 20)]
+    public string sentence;
+}
+
+[CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue")]
+public class Dialogue : ScriptableObject
+{
+    public Image lillian;
+    public Line[] lines;
+
+    //public Image lillian;
+
+    //[TextArea(3, 20)]
+    //public string[] sentences;
 }
--- Flower Hour AR App/Assets/Scripts/DialogueManager.cs
+++ Flower Hour AR App/Assets/Scripts/DialogueManager.cs
@@ -1,20 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class DialogueManager : MonoBehaviour
 {
-    private Queue<string> sentences;
+    public Dialogue dialogue;
+    public Image lillian;
+    public Text textBox;
+    public Button nextButton;
+
+    public string nextScene;
+
+    private int currentDialogueLine = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        sentences = new Queue<string>();
+        DisplayLine();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NextDialogueLine()
     {
-        
+        if (currentDialogueLine < dialogue.lines.Length)
+        {
+            DisplayLine();
+            currentDialogueLine++;
+        }
+        else
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+
+    private void DisplayLine()
+    {
+        Line line = dialogue.lines[currentDialogueLine];
+        Sprite sprite = line.lillian;
+
+        textBox.text = line.ToString();
+        lillian.sprite = sprite;
     }
 }
